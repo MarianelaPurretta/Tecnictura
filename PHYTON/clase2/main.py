95d1b86f86cd250be20c70218bc3f68b96cf23b6
Marianela Purretta <marianela.purretta@yahoo.com>
2023-05-26 12:22:57 -0300

    Clase2 VARIABLES PYTHON, agrego captura y archivo.py

---
+++ PHYTON/clase2/main.py
@@ -0,0 +1,22 @@
+miVariable = 3
+print(miVariable)
+miVariable = "a"
+print(miVariable)
+miVariable = 3.5
+print(miVariable)
+# Variables dinámicas, misma variable distinto tipo de valor
+x = 10
+y = 2
+z = x+y
+print(z)
+print(id(x))
+
+#función ID para conocer la dirección de la casilla de
+# memoria donde se almacena la literal de una variable
+#las literales se escriben x+los ultimos 3 números
+# de su posisción ej: x200
+print(id(y))
+print(id(z))
+
+# CLASE3
+
