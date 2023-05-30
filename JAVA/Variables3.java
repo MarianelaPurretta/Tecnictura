ada5ae72c986ad447d636f558253acd4cd3b82b8
Marianela Purretta <marianela.purretta@yahoo.com>
2023-05-29 22:23:24 -0300

    Añado clase3 JAVA Variables

---
+++ JAVA/Variables3.java
@@ -0,0 +1,66 @@
+
+import java.util.Scanner;
+
+public class Variables2 {
+
+    public static void main(String[] args) {
+        //var-inferencia de tipos Java 
+
+        //var miVariableEntera2 = 10; 
+        //var miVariableCadena2 = "Seguimos estudiando"; 
+        //System.out.println("miVariableCadena2 = " + miVariableCadena2); 
+        //soutv+tab 
+        //System.out.println("miVariableEntera2 = " + miVariableEntera2); 
+        //Al usar + concatena a la literal de la variable 
+        //Reglas para definir variables 
+        //recomendadoCamelCase 
+        //concatenacion 
+        //var usuario = "Osvaldo"; 
+        //var titulo = "Ingeniero"; 
+        //var union = titulo + " " + usuario; 
+        //System.out.println("union = " + union); 
+        //concateno sin espacio, colocamos " " 
+        //si el codigo esta desordenado click der+format*/ 
+        //var a = 8; 
+        //var b = 4; 
+        //System.out.println(a + b);
+        //aca no concatena porque var identifico que 8 es de tipo int (=12) 
+        //si hubiera de izq a der primero un string entonces concatenarÃ­a todo 
+        //no sumari­a.ej:*/ 
+        //var nombre = "Lucas"; 
+        //var a = 8; 
+        //var b = 4; 
+        //System.out.println(nombre + a + b);*/ 
+        //para cambiar prioridad suma por parÃ©ntesis y luego concatena ej: 
+        /*var name = "Lucas"; 
+        var a = 8;     
+        var b = 4; 
+        System.out.println(name + (a + b));*/
+        //Caracteres especiales en Java 
+        /*var nombre = "Natalia"; 
+        System.out.println("Nueva Li­nea: \n" + nombre); 
+        //concatenamos como str y con alt der+-_ hice barra  
+        //invertida \n(salto de linea)  
+        System.out.println("Tabulador: \t" + nombre); 
+        //tabulador: \t, puede ponerse uno o varios para centrar mas 
+        //caracter de retroseso: \b borra un caracter hacia atras 
+        System.out.println("Retroseso: \b" + nombre); 
+        //comilas simple y dobles: 
+        System.out.println("Comillas simples: \'" + nombre + "\'"); 
+
+        System.out.println("Comillas dobles: \"" + nombre + "\"");*/
+        //Clase Scanner 
+        //Scanner entrada = new Scanner(System.in);
+        /*con "new" scanner creamos un objeto (entrada) de la clase scanner 
+        debe estar importada la clase Scanner*/
+ /*System.out.println("Digite su nombre: "); 
+        var usuario2 = entrada.nextLine();//agregamos un mÃ©todo para que el*/
+ /*usuario ingrese valor al objeto creado "nextLine" para leer lÃ­nea 
+        completa de consola, espera que el usuario ingrese info*/
+ /* System.out.println("usuario2 = " + usuario2); 
+        System.out.println("Escriba el ti­tulo: "); 
+        var titulo2 = entrada.nextLine(); 
+        System.out.println("Resultado: " + titulo2 + " " + usuario2);*/
+    }
+
+}
