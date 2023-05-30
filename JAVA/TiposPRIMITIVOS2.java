73b1eebfeec850e4d1293b4f44888968ec3ccdbe
Marianela Purretta <marianela.purretta@yahoo.com>
2023-05-29 22:24:45 -0300

    Añado clase 4 TIPOS PRIMITIVOS JAVA

---
+++ JAVA/TiposPRIMITIVOS2.java
@@ -0,0 +1,39 @@
+  
+public class Tipos1 {
+
+    public static void main(String[] args) {
+        //****Clase4:Tipos Primitivos 
+        /*byte numEnteroByte = 127;
+        System.out.println("numEnteroByte: " + numEnteroByte);
+        System.out.println("Valor minimo del Byte:" + Byte.MIN_VALUE);
+        System.out.println("Valor maximo del Byte:" + Byte.MAX_VALUE);
+        
+        short numEnteroShort = 32767;        
+        System.out.println("numEnteroShort: " + numEnteroShort);
+        System.out.println("Valor minimo del Short" + Short.MIN_VALUE);
+        System.out.println("Valor maximo del Short" + Short.MAX_VALUE);
+
+        int numEnteroInt = 2147483647;
+        System.out.println("numEnteroInt: " + numEnteroInt);
+        System.out.println("Valor minimo del Int" + Integer.MIN_VALUE);
+        System.out.println("Valor maximo del Int" + Integer.MAX_VALUE);
+
+        long numEnteroLong = 9223372036854775807L;
+        System.out.println("numEnteroLong: " + numEnteroLong);
+        System.out.println("Valor minimo del Long" + Long.MIN_VALUE);
+        System.out.println("Valor maximo del Long" + Long.MAX_VALUE);*/
+
+        // Tipos Flotante  
+        float numFloat = 3.4028235E38F;
+        System.out.println("numFloat= " + numFloat);
+        System.out.println("El valor minimo de Float: " + Float.MIN_VALUE);
+        System.out.println("El valor maximo de Float es: " + Float.MAX_VALUE);
+        
+        double numDouble = 1.7976931348623157E308;
+        System.out.println("numDouble = " + numDouble);
+        System.out.println("El valor maximo de double es: " + Double.MAX_VALUE);
+        System.out.println("El valor minimo se double es: " + Double.MIN_VALUE);
+        
+    }
+
+}
