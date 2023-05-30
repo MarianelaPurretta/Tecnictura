d68288d7530fde4c4c4510c0d9052d626f302ad2
Marianela Purretta <marianela.purretta@yahoo.com>
2023-05-29 22:22:01 -0300

    clase2 JAVA Variables, añado archivo.java

---
+++ JAVA/VARIABLES.java
@@ -0,0 +1,22 @@
+
+public class VARIABLES {
+      public static void main(String[] args) { 
+       //tipo entero 
+        int miVariable = 10; 
+        //sout+tab 
+        System.out.println(miVariable); 
+        //reutilizamos la variable y asignamos otro valor 
+        miVariable = 5; 
+        System.out.println(miVariable); 
+        //tipo String es una CLASE empieza con May 
+        String miVariableCadena = "Que despelote Java"; 
+        System.out.println(miVariableCadena); 
+        //reutilizamos 
+        miVariableCadena = "Que no decaigaa"; 
+        System.out.println(miVariableCadena); 
+        //ctrl+clic sobre la variable muestra donde fue creada 
+        
+      
+              
+    }
+}
