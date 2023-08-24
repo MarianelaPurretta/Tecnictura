1195c3c440319fea83d9ee6c851c0ff46dddbe4c
Marianela Purretta <marianela.purretta@yahoo.com>
2023-08-23 23:21:45 -0300

    Clase 2 JAVA Ciclos, Clases

---
+++ JAVA/Leccion3/leccionClasesObjetos/src/Clases/Persona.java
@@ -0,0 +1,10 @@
+
+package Clases;
+
+public class Persona {
+    /*cada clase es una plantilla donde vamos a trabajar, cada clase tiene 
+    un nombre, atributos y métodos. A partir de una clase creamos objetos*/
+    
+    
+    
+}
