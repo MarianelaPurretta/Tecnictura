a9ad7c03ed46e889302ec671f5e6c75c1cf0bc49
Marianela Purretta <marianela.purretta@yahoo.com>
2023-09-20 18:19:26 -0300

    Arreglo por error clase LECCION5

--- PYTHON/LECCION5/FUNCIONES.py
+++
@@ -1,8 +0,0 @@
-#FUNCIONES EN PYTHON
-
-#Definimos una función:
-def mi_funcion(): #dentro de los paréntesis podemos colocar parámetros
-    print('Saludos')
-
-mi_funcion() #Llamamos a la función luego de definirla
-mi_funcion() #Se puede llamar N cantidad de veces
