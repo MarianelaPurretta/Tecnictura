90e1b47c8c28006679a10045e32558f9084c6af9
Marianela Purretta <marianela.purretta@yahoo.com>
2023-09-19 21:21:16 -0300

    Ejercicios PYTHON Clase5 Funciones y ejercicios

---
+++ PYTHON/LECCION5/FUNCIONES.py
@@ -0,0 +1,8 @@
+#FUNCIONES EN PYTHON
+
+#Definimos una función:
+def mi_funcion(): #dentro de los paréntesis podemos colocar parámetros
+    print('Saludos')
+
+mi_funcion() #Llamamos a la función luego de definirla
+mi_funcion() #Se puede llamar N cantidad de veces
