ef47dc47c91c92059dc47a620355185b49c52a86
Marianela Purretta <marianela.purretta@yahoo.com>
2023-10-03 22:50:47 -0300

    Añado Clase 7 JAVASCRIPT

--- JavaScript/leccion02/02-01ConcatenacionDeCadenas.js
+++ JavaScript/leccion02/02-01ConcatenacionDeCadenas.js
@@ -14,4 +14,23 @@ console.log(juntos); // sumó y luego concatenó de Tipo str. "CONTEXTO STR O CA
 nombre += apellido; //tercera forma de concatenar
 console.log(nombre);
 
+//Hoy ya no se usa var, usamos let y const
+let nombre2 = "Mari";
+console.log(nombre2);
 
+
+const apellido2 = "Siri";
+//apellido2 = "Siri"; una constante no puede ser modificada
+console.log(apellido2);
+
+
+let x, y; //similitud con java: declaramos varias variables en una misma línea
+x = 17, y = 21; //asignación de varias variables en la misma línea
+let z = x + y; // Se le asigna a la variable el valor de la asignación
+console.log(z)
+
+let _1num = 31;// no usar números para inicializar una variable key: "value", 
+let rompiendo = "rompe"; //no usar palabras reservadas para variables
+
+console.log(_1num)
+console.log(rompiendo)
